c8f31915ec71c0eb77e79245b6a8f52ad34e0c2e
Ranvijay_Singh <vijaypop12@gmail.com>
2020-11-11 20:53:11 -0800

    express-staticFile

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const http = require("http");
+const morgan = require("morgan");
 const host = "localhost";
 const port = 3000;
 
 const app = express();
+
+app.use(morgan("dev"));
+
+app.use(express.static(__dirname + "/public"));
 
 app.use((req, res, next) => {
   res.statusCode = 200;
